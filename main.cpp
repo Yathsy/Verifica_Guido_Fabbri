b827b2d8f4403526d3657c587de456d20c8e7847
Guido_Fabbri <guido9928@gmail.com>
2017-11-22 05:30:58 +0100

    main.cpp da commentare

--- main.cpp
+++ main.cpp
@@ -1,22 +1,22 @@
 #include <iostream>
-
+//da commentare
 using namespace std;
-
+//da commentare
 int main () {
-    
+     //da commentare
      int contatore = 1;
-     
+     //da commentare
      cout << contatore << endl;
-     
+     //da commentare
      for (int i = 0; i < 9; i++)
      {
          contatore++;
-         
+         //da commentare
          cout << contatore << endl;
          
      }
-     
+     //da commentare
      system ("pause");
-     
+     //da commentare
      return 0;
 }
