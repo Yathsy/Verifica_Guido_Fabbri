0f9a5b75da7a09d4ac8cf6c4a1d532e1538631aa
Guido_Fabbri <guido9928@gmail.com>
2017-11-22 05:23:52 +0100

    aggiunta main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+     int contatore = 1;
+     cout << contatore << endl;
+     for (int i = 0; i < 9; i++)
+     {
+         contatore++;
+         cout << contatore << endl;
+     }
+     system ("pause");
+     return 0;
+}
