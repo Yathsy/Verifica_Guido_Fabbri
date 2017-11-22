86fa718edbe6573159f12b6f6f952288df5064f9
Guido_Fabbri <guido9928@gmail.com>
2017-11-22 05:26:00 +0100

    modifica cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,22 @@
 #include <iostream>
+
 using namespace std;
 
 int main () {
+    
      int contatore = 1;
+     
      cout << contatore << endl;
+     
      for (int i = 0; i < 9; i++)
      {
          contatore++;
+         
          cout << contatore << endl;
+         
      }
+     
      system ("pause");
+     
      return 0;
 }
